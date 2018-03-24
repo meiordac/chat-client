1f851d36bf689692572208868869187165438557
Matias Iordache <matias.iordache@gmail.com>
2018-03-24 16:42:06 +0100

    add user dialog component to choose a username

---
+++ src/app/chat/components/user-dialog/user-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-user-dialog',
+  templateUrl: './user-dialog.component.html',
+  styleUrls: ['./user-dialog.component.css']
+})
+export class UserDialogComponent  {
+
+  constructor(
+    public dialogRef: MatDialogRef<UserDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
