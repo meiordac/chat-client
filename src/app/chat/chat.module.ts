430f12c30060068e8b4a6681f3ce1db7bd63d58e
Matias Iordache <matias.iordache@gmail.com>
2018-03-24 13:08:02 +0100

    provide socket service in chat module, add button to send a message calling the function

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatComponent } from './components/chat.component';
+import { SocketService } from './services/socket.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
+  exports: [
+    ChatComponent
+  ],
+  providers: [SocketService],
   declarations: [ChatComponent]
 })
 export class ChatModule { }
