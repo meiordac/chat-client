989b248dd83d837bdc7152132f0ed795b7541826
Matias Iordache <matias.iordache@gmail.com>
2018-03-24 22:35:29 +0100

    add on error, on joined and on users changed

--- src/app/chat/services/socket.service.ts
+++ src/app/chat/services/socket.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
+import { of } from 'rxjs/observable/of';
 
 import * as socketIo from 'socket.io-client';
 import { SocketEvent } from '../models/event';
 import { ChatMessage } from '../models/message';
+import { User } from '../models/user';
 
 const SERVER_URL = 'http://localhost:8080';
 
@@ -16,7 +18,7 @@ export class SocketService {
         this.socket = socketIo(SERVER_URL);
     }
 
-    public send(message: any): void {
+    public send(message: ChatMessage): void {
         this.socket.emit('message', message);
     }
 
@@ -26,9 +28,30 @@ export class SocketService {
         });
     }
 
+    public onUsersChanged(): Observable<User[]> {
+        return new Observable<User[]>(observer => {
+            this.socket.on('users', (data: User[]) => observer.next(data));
+        });
+    }
+
+    public onJoined(): Observable<string> {
+        return new Observable<string>(observer => {
+            this.socket.on('join', (data: string) => observer.next(data));
+        });
+    }
+
     public onEvent(event: SocketEvent): Observable<any> {
         return new Observable<SocketEvent>(observer => {
             this.socket.on(event, () => observer.next());
+        });
+    }
+
+    public onError(): Observable<any> {
+          return new Observable<SocketEvent>(observer => {
+            this.socket.on('error', (error) => {
+                console.log(error);
+                observer.next(error);
+            });
         });
     }
 }
