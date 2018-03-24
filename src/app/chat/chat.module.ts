7d917d760d3e02d09ea55947644594c995e92b1e
Matias Iordache <matias.iordache@gmail.com>
2018-03-24 14:42:42 +0100

    display messages and add input for sending new messages, add styles

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatComponent } from './components/chat.component';
 import { SocketService } from './services/socket.service';
-import { MatCardModule } from '@angular/material/card';
+import { MatCardModule, MatIconModule, MatInputModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatCardModule
+    FormsModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule
   ],
   exports: [
     ChatComponent
