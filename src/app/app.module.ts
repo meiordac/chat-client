fa460a359a411778215269c09db7c9f6b707fcc9
Matias Iordache <matias.iordache@gmail.com>
2018-04-08 17:51:48 +0200

    upgrade angular, install angular service worker add ngsw-config manifest, and register on production

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { MatIconModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ChatModule } from './chat/chat.module';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -12,6 +14,7 @@ import { ChatModule } from './chat/chat.module';
   ],
   imports: [
     BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
     MatIconModule,
     BrowserAnimationsModule,
     ChatModule
